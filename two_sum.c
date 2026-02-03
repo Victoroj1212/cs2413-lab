73d89c487947dd047c8db1c1fc53e4ef8832c094
Victoroj1212 <victoradeoye1212@gmail.com>
2026-02-03 10:02:01 -0600

    Add files via upload

--- two_sum.c
+++ two_sum.c
@@ -1,19 +1,18 @@
 #include "two_sum.h"
 
 int two_sum(const int* nums, int n, int target, int* out_i, int* out_j) {
-    // basic safety check
+    
     if (!nums || !out_i || !out_j || n < 2) return 0;
 
-    // check every pair
     for (int i = 0; i < n - 1; i++) {
         for (int j = i + 1; j < n; j++) {
             if (nums[i] + nums[j] == target) {
                 *out_i = i;
                 *out_j = j;
-                return 1;   // found a solution
+                return 1;  
             }
         }
     }
 
-    return 0; // no solution
+    return 0; 
 }
