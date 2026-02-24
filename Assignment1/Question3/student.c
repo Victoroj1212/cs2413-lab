334e9e08bf1160b9327ded21187a5e52848f4e60
root <root@Victoroj.localdomain>
2026-02-24 08:17:09 -0600

    Completed previous labs and assignment

--- Assignment1/Question3/student.c
+++ Assignment1/Question3/student.c
@@ -1,33 +1,28 @@
 #include "Student.h"
 #include <stdlib.h>
 
-//You are given a large integer represented as an integer array digits, where:
-
-//digits[i] is the i-th digit of the integer
-//digits are ordered from most significant to least significant
-//there are no leading 0s (except the number 0 itself in some of our tests)
-//Goal: Add 1 to the number and return the resulting digits array.
-
-//Example 1:
-//Input: digits = [4,3,2,1]
-//Output: [4,3,2,2]
-//Explanation: The array represents the integer 4321.
-//Incrementing by one gives 4321 + 1 = 4322.
-//Thus, the result should be [4,3,2,2].
-
-//Example 2:
-//Input: digits = [9]
-//Output: [1,0]
-//Explanation: The array represents the integer 9.
-//Incrementing by one gives 9 + 1 = 10.
-//Thus, the result should be [1,0].
-
-//Return value (int*): the output digits array.
-//Output parameter (returnSize): set *returnSize to the number of digits in the returned array.
-
+// digits: array of length digitsSize
+// returns new array, sets *returnSize
 int* plusOne(int* digits, int digitsSize, int* returnSize) {
-    // TODO: implement
+    // work from the end like normal addition
+    for (int i = digitsSize - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i] += 1;
 
-    
+            int* out = (int*)malloc(sizeof(int) * (size_t)digitsSize);
+            for (int k = 0; k < digitsSize; k++) out[k] = digits[k];
+
+            *returnSize = digitsSize;
+            return out;
+        }
+        digits[i] = 0;
+    }
+
+    // if we got here, it was all 9s -> need one extra digit
+    int* out = (int*)malloc(sizeof(int) * (size_t)(digitsSize + 1));
+    out[0] = 1;
+    for (int i = 1; i <= digitsSize; i++) out[i] = 0;
+
+    *returnSize = digitsSize + 1;
+    return out;
 }
-
